1a2e91e2f4314000eac533254cb4653bc7ccafe1
md:robin mia <mdrobinmiaro1@gmail.com>
2023-05-03 21:38:24 +0600

    added id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors ())
+app.use(cors())
 const categories = require('./data/categories.json')
 
 app.get('/', (req, res) => {
@@ -12,6 +12,13 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedNews = categories.find(n => n._id === id)
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
