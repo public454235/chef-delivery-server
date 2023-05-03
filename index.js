d1ac954af3fef7ee06c4db4e5183bac98db4aae8
md:robin mia <mdrobinmiaro1@gmail.com>
2023-05-03 13:15:26 +0600

    categories json

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors ())
+const categories = require('./data/categories.json')
 
 app.get('/', (req, res) => {
     res.send('Dragon is running')
 });
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
 
 app.listen(port, () => {
     console.log(`Dragon API is running on port: ${port}`)
